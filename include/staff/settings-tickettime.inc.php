76a030249445f7e4ba292adbe2df57c26ff0099d
xerardoo <gerardoodash2413@gmail.com>
2015-01-24 11:28:36 -0700

    Se tradujo la ventana de Panel Agente > Tiempo > configuracion, y se agrego a .gitignore archivos del IDE

--- include/staff/settings-tickettime.inc.php
+++ include/staff/settings-tickettime.inc.php
@@ -3,7 +3,7 @@
 
 if(!defined('OSTADMININC') || !$thisstaff || !$thisstaff->isAdmin() || !$config) die('Access Denied');
 ?>
-<h2><?php echo __('Ticket Time Settings');?></h2>
+<h2><?php echo __('Configuracion');?></h2>
 <form action="settings.php?t=tickettime" method="post" id="save">
 <?php csrf_token(); ?>
 <input type="hidden" name="t" value="tickettime" >
@@ -11,35 +11,35 @@ if(!defined('OSTADMININC') || !$thisstaff || !$thisstaff->isAdmin() || !$config)
     <thead>
         <tr>
             <th colspan="2">
-                <h4><?php echo __('Ticket Time Settings');?></h4>
-                <em><?php echo __("Enabling these options allow you to add time to you tickets.");?></em>
+                <h4><?php echo __('Configuracion del Tiempo en Tickets');?></h4>
+                <em><?php echo __("Activando estas opciones permites agregar tiempo a tus tickets.");?></em>
             </th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td width="180"><?php echo __('Client Time Status'); ?>:</td>
+            <td width="180"><?php echo __('Estado del Tiempo Invertido:'); ?>:</td>
             <td>
                 <input type="checkbox" name="isclienttime" value="1" <?php echo $config['isclienttime']?'checked="checked"':''; ?>>
-                <?php echo __('Enable Client Time View'); ?>
+                <?php echo __('Activar Ver el Tiempo al Cliente'); ?>
                 &nbsp;<font class="error">&nbsp;<?php echo $errors['isclienttime']; ?></font>
                 <i class="help-tip icon-question-sign" href="#"></i>
             </td>
         </tr>
         <tr>
-            <td width="180"><?php echo __('Total Ticket Time');?>:</td>
+            <td width="180"><?php echo __('Tiempo Invertido Total del Ticket:');?>:</td>
             <td>
                 <input type="checkbox" name="istickettime" value="1" <?php echo $config['istickettime']?'checked="checked"':''; ?> >
-                <?php echo __('Enable Adding Time to Tickets via Actions'); ?>
+                <?php echo __('Activer Agregar Tiempo Invertido a los Tickets Via Accion.'); ?>
                 &nbsp;<font class="error">&nbsp;<?php echo $errors['istickettime']; ?></font>
                 <i class="help-tip icon-question-sign" href="#"></i>
             </td>
         </tr>
 		<tr>
-            <td width="180"><?php echo __('Thread Ticket Time');?>:</td>
+            <td width="180"><?php echo __('Tiempo del Hilo del Ticket:');?>:</td>
             <td>
                 <input type="checkbox" name="isthreadtime" value="1" <?php echo $config['isthreadtime']?'checked="checked"':''; ?> >
-                <?php echo __('Enable Adding Time to Tickets via Threads'); ?>
+                <?php echo __('Activer Agregar Tiempo Invertido a los Tickets via Hilos'); ?>
                 &nbsp;<font class="error">&nbsp;<?php echo $errors['isthreadtime']; ?></font>
                 <i class="help-tip icon-question-sign" href="#"></i>
             </td>
