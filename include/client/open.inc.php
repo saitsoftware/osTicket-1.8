1fc7ce6aa9c0eed5438518d10a4d542cb676c3cf
xerardoo <gerardoo_2413@hotmail.com>
2015-02-10 09:32:45 -0700

    --cambio  mensaje de justpaste.it

--- include/client/open.inc.php
+++ include/client/open.inc.php
@@ -99,7 +99,7 @@ if ($info['topicId'] && ($topic=Topic::lookup($info['topicId']))) {
     </tr>
     <?php
     } ?>
-    <tr><td colspan=2>&nbsp;Nota: Para anexar cualquier imagen o documento de un tamaño superior a 2 Mb, favor de usar la herramienta <a href="https://justpaste.it/">JustPaste.it</a>.<br></td></tr>
+    <tr><td colspan=2>&nbsp;<b>Nota:</b> Para anexar cualquier imagen o documento de un tamaño superior a 1 Mb, favor de usar la herramienta <a href="https://justpaste.it/" target="_blank">JustPaste.it</a>.<br></td></tr>
     </tbody> 
   </table>
 <hr/>
